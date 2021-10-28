dc6291dd441eeafbe31556ba9fd1e83dc9b6d75a
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-10-28 16:59:32 -0700

    wow

--- src/Models/users.js
+++ src/Models/users.js
@@ -5,42 +5,47 @@ const SECRET = process.env.SECRET || 'secretstringfortesting';
 const bcrypt = require('bcrypt');
 
 const Users = (sequelize, DataTypes) => {
-  const usersTable = sequelize.define('Users', {
-    username: {
-      type: DataTypes.STRING,
-      required: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      required: true,
-    },
-    role: {
-      type: DataTypes.ENUM( 'guest', 'user', 'admin', ),
-      defaultValue: 'guest',
-      required: true,
-    },
-    token: {
-      type:DataTypes.VIRTUAL,
-      get() {
-        return jwt.sign({
-          username: this.username
-        }, SECRET)},
-      set(tokenObj){
-        let token = jwt.sign(tokenObj, SECRET);
-        return token;
-      },
-    },
-    capabilities: {
-      type: DataTypes.VIRTUAL,
-      get() {
-        const acl = {
-          guest: ['read'],
-          user: ['read', 'create', 'update'],
-          admin: ['read', 'create', 'update', 'delete'],
-        }
-        return acl[this.role];
-      }
-    },
+	const usersTable = sequelize.define('Users', {
+		username: {
+			type: DataTypes.STRING,
+			required: true,
+		},
+		password: {
+			type: DataTypes.STRING,
+			required: true,
+		},
+		role: {
+			type: DataTypes.ENUM('guest', 'user', 'admin'),
+			defaultValue: 'guest',
+			required: true,
+		},
+		token: {
+			type: DataTypes.VIRTUAL,
+			get() {
+				return jwt.sign(
+					{
+						username: this.username,
+					},
+					SECRET
+				);
+			},
+			set(tokenObj) {
+				let token = jwt.sign(tokenObj, SECRET);
+				return token;
+			},
+		},
+		capabilities: {
+			type: DataTypes.VIRTUAL,
+			get() {
+				const acl = {
+					guest: ['read'],
+					user: ['read', 'create', 'update'],
+					admin: ['read', 'create', 'update', 'delete'],
+				};
+				return acl[this.role];
+			},
+		},
+	});
 
 	usersTable.beforeCreate(async (user) => {
 		let encryptPassword = await bcrypt.hash(user.password, 10);
@@ -56,7 +61,6 @@ const Users = (sequelize, DataTypes) => {
 		throw new Error('Not Authenticated');
 	};
 
-<<<<<<< HEAD
 	usersTable.authenticateToken = async function (token) {
 		try {
 			const parsedToken = jwt.verify(token, SECRET);
@@ -73,28 +77,5 @@ const Users = (sequelize, DataTypes) => {
 	};
 	return usersTable;
 };
-=======
-  usersTable.authenticateBasic = async function (username, password) {
-    let parsedUser = await this.findOne({ where: { username}});
-    let isValidPassword = await bcrypt.compare(password, parsedUser.password);
-    if(isValidPassword) {
-      return parsedUser;
-    } throw new Error('Not Authenticated');
-  };
-  usersTable.authenticateToken = async function (token) {
-    try{
-      const parsedToken = jwt.verify(token, SECRET);
-      const user = await this.findOne({where: {username: parsedToken.username}});
-      if(user){
-        return user;
-      }
-      throw new Error('User Not Found');
-    } catch (e) {
-      throw new Error(e.message);
-    }
-  };
-  return usersTable;
-}
->>>>>>> main
 
 module.exports = Users;
