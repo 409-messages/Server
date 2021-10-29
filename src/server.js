9e39bcb570953f9438d9f49e172c2459c13683e6
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-10-29 08:31:21 -0700

    profiles commented out

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.use(logger);
 // Route-calls
 app.use(userRoutes);
 app.use('/api/secure', messagesRoutes);
-app.use('/api/secure', profileRoutes);
+// app.use('/api/secure', profileRoutes);
 
 // Error-calls
 app.use(err404);
